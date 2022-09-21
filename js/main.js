c36d6cb464c82306c4f944be37d08b31d0eb3bf3
TFuray <pk.master881@gmail.com>
2022-09-21 12:31:13 -0700

    added auto deal options and fixed up logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
 let deckId = ''
 let remaining = document.querySelector('#remaining')
 let count = 0
+let remainingNum
+let intervalID
+const slowBtn = document.getElementById("slow")
+const medBtn = document.getElementById("med")
+const fastBtn = document.getElementById("fast")
+
 
 
 function shuffle() {
     let select = document.getElementById('deckCount')
-    let option = select.options[select.selectedIndex]
-    console.log(option.value);
-    let url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${option.value}`
+    // let option = select.options[select.selectedIndex]
+    count = 0
+    let url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`
     fetch(url)
         .then(res => res.json())
         .then(data => {
             console.log(data);
             deckId = data.deck_id
             remaining.textContent = data.remaining
+            remainingNum = data.remaining
         })
         .catch(err => {
             console.log(`error ${err}`);
@@ -33,21 +40,36 @@ function nextCard() {
             keepCount(cardVal)
             document.querySelector('h4').innerText = " " + ` ${count}`
             
+            if(cardsRemaining == 0){
+                clearInterval(intervalID)
+                shuffle()
+                document.querySelector('#currentCard').src = 'assets/backCard.png'
+                    
+            }
+
         })
         .catch(err => {
             console.log(`error ${err}`)
         })
 }
 
+// function autoDeal(speed) {
+//     if(speed == 'slow'){
+//         while (cardsRemaining > 0){
+
+//         }
+//     }
+// }
+
 function keepCount(val) {
-    
-    if(val < 7){
+
+    if (val < 7) {
         count += 1
         return count
-    }else if(val > 9){
+    } else if (val > 9) {
         count -= 1
         return count
-    }else return count
+    } else return count
 }
 
 function convertToNum(val) {
@@ -64,8 +86,26 @@ function convertToNum(val) {
     }
 }
 
+shuffle()
 
-
-document.querySelector('#submit').addEventListener('click', shuffle)
+// document.querySelector('#submit').addEventListener('click', shuffle)
 
 document.querySelector('#nextCard').addEventListener('click', nextCard)
+
+slowBtn.addEventListener("click", function () {
+    intervalID = setInterval(function() {
+        nextCard()
+    }, 1000)
+})
+
+medBtn.addEventListener("click", function () {
+    intervalID = setInterval(function () {
+        nextCard()
+    }, 600)
+})
+
+fastBtn.addEventListener("click", function () {
+    intervalID = setInterval(function () {
+        nextCard()
+    }, 350)
+})
